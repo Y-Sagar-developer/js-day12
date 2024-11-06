b02434c29490539a3069d94da51e1d40c55eb76b
Y-Sagar-developer <sagaryarra987@gmail.com>
2024-11-06 12:51:12 +0530

    complete

---
+++ js-day12-task.js
@@ -0,0 +1,28 @@
+// task:
+// ------
+// var aos = ["mom", "js", "malayalam", "html", "css", "dad"];
+
+// // op=["mom","dad","malayalam"]
+
+// using for/while and do-while loop
+
+// Answer:
+
+
+var aos = ["mom", "js", "malayalam", "html", "racecar", "css", "dad", "madam"];
+output = [];
+
+for (i = 0; i < aos.length; i++) {
+  var x = aos[i];
+  var y = "";
+  for (j = x.length - 1; j >= 0; j--) {
+    y += x[j];
+  }
+  if (x === y) {
+    output.push(x);
+  }
+}
+console.log(output);
+
+output
+[ 'mom', 'malayalam', 'racecar', 'dad', 'madam' ]
